64560ed7e45a53811e846225ef8981d51bb86a02
AntoCast18 <antonellacastagna00@gmail.com>
2022-05-07 13:42:18 -0300

    first commit

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  
+    
   title:string = 'Lista del dia';
-  
-  constructor() { }
+  showAddTask: boolean = true;
+  subscription?: Subscription;
+
+  constructor(
+      private uiService:UiService
+  ) { 
+      this.subscription = this.uiService.onToggle()
+                                .subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask(){ 
-    console.log("toggleAddTask!");
+   this.uiService.toggleAddTask();
   }
-
 }
